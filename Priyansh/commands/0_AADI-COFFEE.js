c24554e337b21624e208382849c4ff16af756c68
HasenaMalik <159886409+HasenaMalik@users.noreply.github.com>
2025-01-16 17:14:39 +0530

    Update 0_AADI-COFFEE.js

--- Priyansh/commands/0_AADI-COFFEE.js
+++ Priyansh/commands/0_AADI-COFFEE.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "Coffee",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "ZOYA BABU", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "Coffee",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("Coffee")==0 || event.body.indexOf("coffee")==0 || event.body.indexOf("COFFEE ")==0 || event.body.indexOf("cofi")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐎𝐅𝐅𝐄𝐄 \n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐙𝐨𝐲𝐚 𝐛𝐚𝐛𝐮 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐎𝐅𝐅𝐄𝐄 \n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Pinterest-boomerang.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
