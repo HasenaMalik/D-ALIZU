9d9558a7dcccb2334ac56e67af27cd4debbaefdc
HasenaMalik <159886409+HasenaMalik@users.noreply.github.com>
2025-01-16 17:39:44 +0530

    Update 0_AADI-PEPSI.js

--- Priyansh/commands/0_AADI-PEPSI.js
+++ Priyansh/commands/0_AADI-PEPSI.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "pepsi",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "ZOYA BABU", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "pepsi",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("pepsi")==0 || event.body.indexOf("Pepsi")==0 || event.body.indexOf("PEPSI")==0 || event.body.indexOf("Coldaring")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐄𝐏𝐒𝐈\n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐙𝐨𝐲𝐚 𝐛𝐚𝐛𝐮 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐄𝐏𝐒𝐈\n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Pin_di_Shoaib_nazir_su_Quick_Saves(360P).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
