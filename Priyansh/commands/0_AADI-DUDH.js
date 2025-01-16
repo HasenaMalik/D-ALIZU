6c3e7c6b3c7c81ef33671ab0f5c76c76fd1739c3
HasenaMalik <159886409+HasenaMalik@users.noreply.github.com>
2025-01-16 17:17:44 +0530

    Update 0_AADI-DUDH.js

--- Priyansh/commands/0_AADI-DUDH.js
+++ Priyansh/commands/0_AADI-DUDH.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "dudh",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "ZOYA BABU", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "dudh",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("dudh")==0 || event.body.indexOf("DUDH")==0 || event.body.indexOf("DHUD")==0 || event.body.indexOf("Dudh")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍 𝐊 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐃𝐮𝐝𝐇 \n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐙𝐎𝐘𝐀 𝐊𝐇𝐀𝐍 𝐊 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐃𝐮𝐝𝐇 \n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/(720p).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
