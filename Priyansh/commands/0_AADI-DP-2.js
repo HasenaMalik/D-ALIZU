277dcb53d78417b885059d2dc68da580a711093e
HasenaMalik <159886409+HasenaMalik@users.noreply.github.com>
2025-01-16 17:16:18 +0530

    Update 0_AADI-DP-2.js

--- Priyansh/commands/0_AADI-DP-2.js
+++ Priyansh/commands/0_AADI-DP-2.js
@@ -2,8 +2,8 @@ module.exports.config = {
   name: "dp2",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "AADI BABU",
-  description: "THIS BOT IS MADE BY AADI BABU",
+  credits: "ZOYA BABU",
+  description: "THIS BOT IS MADE BY ZOYA BABU",
   commandCategory: "PROFILE DP 2",
   usages: "SELF DP AND MENTION",
   dependencies: {
@@ -103,4 +103,4 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
     }
  
  
-//MADE BY AADI BABU 
+//MADE BY ZOYA BABU 
