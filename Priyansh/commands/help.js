3d4723aab675d74f58a41fd2f18228f1b2d89102
HasenaMalik <159886409+HasenaMalik@users.noreply.github.com>
2025-01-27 23:55:29 +0530

    Update help.js

--- Priyansh/commands/help.js
+++ Priyansh/commands/help.js
@@ -107,7 +107,7 @@ const first = numberOfOnePage * page - numberOfOnePage;
 
     const siu = `𝐎𝐰𝐧𝐞𝐫 ➻   𝐙𝐨𝐘𝐚 𝐛𝐚𝐛𝐮`;
 
- const text = `\nPAGE 𒁍 (${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)})\n\nOR COMMAND KE LIYE HELP-2 TYPE KRO\n\n𝐎𝐰𝐧𝐞𝐫 ➻   𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮\n\n● ──────────────────── ●\n\n𝐌𝐘 𝐎𝐰𝐧𝐞𝐑 𝐙𝐨𝐘𝐚 𝐁𝐚𝐁𝐮 .... < 𝐄𝐃𝐈𝐓 > .... 𝐘𝐞 𝐁𝐨𝐓 𝐒𝐢𝐫𝐅 𝐎𝐰𝐧𝐞𝐑 𝐊 𝐋𝐢𝐘𝐞 𝐇 .... 𝐌𝐮𝐣𝐇𝐞 𝐀𝐚𝐏 𝐋𝐨𝐆𝐨 𝐊𝐨 𝐇𝐚𝐬𝐚𝐍𝐞 𝐊 𝐋𝐢𝐘𝐞 𝐁𝐚𝐧𝐘𝐚 𝐆𝐲𝐚 𝐇 𝐓𝐨𝐇 𝐇𝐚𝐩𝐩𝐘 𝐑𝐞𝐇𝐚𝐍𝐀 𝐀𝐩𝐩𝐤𝐚 𝐀𝐩𝐩𝐧𝐚 𝐎𝐰𝐧𝐞𝐑 𝐀𝐚𝐝𝐢 𝐁𝐚𝐁𝐮\n\n● ─────────────────── ●\n\n`;
+ const text = `\nPAGE 𒁍 (${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)})\n\nOR COMMAND KE LIYE HELP-2 TYPE KRO\n\n𝐎𝐰𝐧𝐞𝐫 ➻   𝐙𝐨𝐘𝐚 𝐛𝐚𝐛𝐮\n\n● ──────────────────── ●\n\n𝐌𝐘 𝐎𝐰𝐧𝐞𝐑 𝐙𝐨𝐘𝐚 𝐁𝐚𝐁𝐮 .... < 𝐄𝐃𝐈𝐓 > .... 𝐘𝐞 𝐁𝐨𝐓 𝐒𝐢𝐫𝐅 𝐎𝐰𝐧𝐞𝐑 𝐊 𝐋𝐢𝐘𝐞 𝐇 .... 𝐌𝐮𝐣𝐇𝐞 𝐀𝐚𝐏 𝐋𝐨𝐆𝐨 𝐊𝐨 𝐇𝐚𝐬𝐚𝐍𝐞 𝐊 𝐋𝐢𝐘𝐞 𝐁𝐚𝐧𝐘𝐚 𝐆𝐲𝐚 𝐇 𝐓𝐨𝐇 𝐇𝐚𝐩𝐩𝐘 𝐑𝐞𝐇𝐚𝐍𝐀 𝐀𝐩𝐩𝐤𝐚 𝐀𝐩𝐩𝐧𝐚 𝐎𝐰𝐧𝐞𝐑 𝐀𝐚𝐝𝐢 𝐁𝐚𝐁𝐮\n\n● ─────────────────── ●\n\n`;
     var link = [
 "https://i.imgur.com/FTU6dwh.jpeg", 
 "https://i.imgur.com/FTU6dwh.jpeg"
