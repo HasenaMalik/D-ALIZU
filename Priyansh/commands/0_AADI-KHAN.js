d863bef3b0a71fd3d74ff89b0e52442070adceae
HasenaMalik <159886409+HasenaMalik@users.noreply.github.com>
2025-02-18 13:31:17 +0530

    Update 0_AADI-KHAN.js

--- Priyansh/commands/0_AADI-KHAN.js
+++ Priyansh/commands/0_AADI-KHAN.js
@@ -19,7 +19,7 @@ react.includes("BEWAFA") ||
 react.includes("Boɭtīī") ||     
 react.includes("Ɓɘwʌʆʌ")) {
     var msg = {
-        body: `𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐓𝐨 𝐁𝐨𝐭 𝐢𝐃 🤖\n\n● ────────────────── ●\n\n𝐌𝐘 𝐎𝐰𝐧𝐞𝐑 𝐙𝐨𝐘𝐚 𝐁𝐚𝐁𝐮 .... < 𝐄𝐃𝐈𝐓 > .... 𝐘𝐞 𝐁𝐨𝐓 𝐒𝐢𝐫𝐅 𝐎𝐰𝐧𝐞𝐑 𝐊 𝐋𝐢𝐘𝐞 𝐇 .... 𝐌𝐮𝐣𝐇𝐞 𝐀𝐚𝐏 𝐋𝐨𝐆𝐨 𝐊𝐨 𝐇𝐚𝐬𝐚𝐍𝐞 𝐊 𝐋𝐢𝐘𝐞 𝐁𝐚𝐧𝐘𝐚 𝐆𝐲𝐚 𝐇 𝐓𝐨𝐇 𝐇𝐚𝐩𝐩𝐘 𝐑𝐞𝐇𝐚𝐍𝐀 𝐀𝐩𝐩𝐤𝐚 𝐀𝐩𝐩𝐧𝐚 𝐎𝐰𝐧𝐞𝐑 𝐙𝐨𝐲𝐚 𝐁𝐚𝐁𝐮 https://www.facebook.com/share/1AuqSzgHry/𝐊𝐢𝐬𝐢 𝐁𝐡𝐢 𝐓𝐚𝐇𝐚𝐫𝐚 𝐊𝐢 𝐇𝐞𝐥𝐩 𝐋𝐚𝐧𝐢 𝐇𝐨 𝐌𝐞𝐒𝐒𝐠 𝐊𝐚𝐑𝐞 𝐎𝐰𝐧𝐞𝐫 ➻  𝐙𝐨𝐲𝐚 𝐁𝐚𝐛𝐮 https://www.facebook.com/share/1AuqSzgHry/𝐎𝐰𝐧𝐞𝐫 ➻  𝐙𝐨𝐲𝐚 𝐛𝐚𝐛𝐮`,attachment: fs.createReadStream(__dirname + `/noprefix/Picsart_24-12-13_23-13-49-826.jpg`)
+        body: `𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐓𝐨 𝐁𝐨𝐭 𝐢𝐃 🤖\n\n● ────────────────── ●\n\n𝐌𝐘 𝐎𝐰𝐧𝐞𝐑 𝐙𝐨𝐘𝐚 𝐁𝐚𝐁𝐮 .... < 𝐄𝐃𝐈𝐓 > .... 𝐘𝐞 𝐁𝐨𝐓 𝐒𝐢𝐫𝐅 𝐎𝐰𝐧𝐞𝐑 𝐊 𝐋𝐢𝐘𝐞 𝐇 .... 𝐌𝐮𝐣𝐇𝐞 𝐀𝐚𝐏 𝐋𝐨𝐆𝐨 𝐊𝐨 𝐇𝐚𝐬𝐚𝐍𝐞 𝐊 𝐋𝐢𝐘𝐞 𝐁𝐚𝐧𝐘𝐚 𝐆𝐲𝐚 𝐇 𝐓𝐨𝐇 𝐇𝐚𝐩𝐩𝐘 𝐑𝐞𝐇𝐚𝐍𝐀 𝐀𝐩𝐩𝐤𝐚 𝐀𝐩𝐩𝐧𝐚 𝐎𝐰𝐧𝐞𝐑 𝐙𝐨𝐲𝐚 𝐁𝐚𝐁𝐮 https://www.facebook.com/share/1AuqSzgHry/𝐊𝐢𝐬𝐢 𝐁𝐡𝐢 𝐓𝐚𝐇𝐚𝐫𝐚 𝐊𝐢 𝐇𝐞𝐥𝐩 𝐋𝐚𝐧𝐢 𝐇𝐨 𝐌𝐞𝐒𝐒𝐠 𝐊𝐚𝐑𝐞 𝐎𝐰𝐧𝐞𝐫 ➻  𝐙𝐨𝐲𝐚 𝐁𝐚𝐛𝐮 https://www.facebook.com/share/1AuqSzgHry/𝐎𝐰𝐧𝐞𝐫 ➻  𝐙𝐨𝐲𝐚 𝐛𝐚𝐛𝐮`,attachment: fs.createReadStream(__dirname + `/noprefix/Picsart_https://i.imgur.com/rEzMBkr.jpeg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("💋", event.messageID, (err) => {}, true)
